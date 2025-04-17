600671451220b9239f02e83ce301cb84273c89da
Kaito Udagawa <umireon@gmail.com>
2025-04-18 03:31:11 +0900

    Fix

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -9,63 +9,63 @@ import type { CommandResponse } from '@discord/embedded-app-sdk';
 type Auth = CommandResponse<'authenticate'>;
 let auth: Auth;
 
-export const discordSdk = new DiscordSDK(import.meta.env.VITE_CLIENT_ID);
+const discordSdk = new DiscordSDK(import.meta.env.VITE_CLIENT_ID);
 
 async function setupDiscordSdk() {
 	await discordSdk.ready();
 
 	// Authorize with Discord Client
-	const { code } = await discordSdk.commands.authorize({
-		client_id: import.meta.env.VITE_CLIENT_ID,
-		response_type: 'code',
-		state: '',
-		prompt: 'none',
-		// More info on scopes here: https://discord.com/developers/docs/topics/oauth2#shared-resources-oauth2-scopes
-		scope: [
-			// Activities will launch through app commands and interactions of user-installable apps.
-			// https://discord.com/developers/docs/tutorials/developing-a-user-installable-app#configuring-default-install-settings-adding-default-install-settings
-			'applications.commands',
+	// const { code } = await discordSdk.commands.authorize({
+	// 	client_id: import.meta.env.VITE_CLIENT_ID,
+	// 	response_type: 'code',
+	// 	state: '',
+	// 	prompt: 'none',
+	// 	// More info on scopes here: https://discord.com/developers/docs/topics/oauth2#shared-resources-oauth2-scopes
+	// 	scope: [
+	// 		// Activities will launch through app commands and interactions of user-installable apps.
+	// 		// https://discord.com/developers/docs/tutorials/developing-a-user-installable-app#configuring-default-install-settings-adding-default-install-settings
+	// 		'applications.commands',
 
-			// "applications.builds.upload",
-			// "applications.builds.read",
-			// "applications.store.update",
-			// "applications.entitlements",
-			// "bot",
-			'identify',
-			// "connections",
-			// "email",
-			// "gdm.join",
-			'guilds',
-			// "guilds.join",
-			'guilds.members.read',
-			// "messages.read",
-			// "relationships.read",
-			// 'rpc.activities.write',
-			// "rpc.notifications.read",
-			// "rpc.voice.write",
-			'rpc.voice.read',
-			// "webhook.incoming",
-		],
-	});
+	// 		// "applications.builds.upload",
+	// 		// "applications.builds.read",
+	// 		// "applications.store.update",
+	// 		// "applications.entitlements",
+	// 		// "bot",
+	// 		'identify',
+	// 		// "connections",
+	// 		// "email",
+	// 		// "gdm.join",
+	// 		'guilds',
+	// 		// "guilds.join",
+	// 		'guilds.members.read',
+	// 		// "messages.read",
+	// 		// "relationships.read",
+	// 		// 'rpc.activities.write',
+	// 		// "rpc.notifications.read",
+	// 		// "rpc.voice.write",
+	// 		'rpc.voice.read',
+	// 		// "webhook.incoming",
+	// 	],
+	// });
 
 	// Retrieve an access_token from your activity's server
 	// /.proxy/ is prepended here in compliance with CSP
 	// see https://discord.com/developers/docs/activities/development-guides#construct-a-full-url
-	const response = await fetch('/.proxy/api/token', {
-		method: 'POST',
-		headers: {
-			'Content-Type': 'application/json',
-		},
-		body: JSON.stringify({
-			code,
-		}),
-	});
-	const { access_token } = await response.json();
+	// const response = await fetch('/.proxy/api/token', {
+	// 	method: 'POST',
+	// 	headers: {
+	// 		'Content-Type': 'application/json',
+	// 	},
+	// 	body: JSON.stringify({
+	// 		code,
+	// 	}),
+	// });
+	// const { access_token } = await response.json();
 
-	// Authenticate with Discord client (using the access_token)
-	auth = await discordSdk.commands.authenticate({
-		access_token,
-	});
+	// // Authenticate with Discord client (using the access_token)
+	// auth = await discordSdk.commands.authenticate({
+	// 	access_token,
+	// });
 
 	if (auth == null) {
 		throw new Error('Authenticate command failed');
