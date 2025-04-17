a55bb67ecfd1df197ec30cbdacc9cc5016ccd8c2
Kaito Udagawa <umireon@gmail.com>
2025-04-18 04:06:01 +0900

    Revert "Fix"
    This reverts commit fb13e9adcc58abe1b51b2aa9f0e9bd5a4f906b57.

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,4 +1,4 @@
-import { useReducer, useState } from "react";
+import { useState } from "react";
 
 import { DiscordSDK } from '@discord/embedded-app-sdk';
 
@@ -16,110 +16,12 @@ function say(text: string) {
   speechSynthesis.speak(utterance);
 }
 
-interface TimerState {
-  readonly duration: number;
-  readonly matchStartMillis: number;
-  readonly remainingMillis: number;
-  readonly calledMillis: number;
-  readonly isRunning: boolean;
-}
-
-interface TimerStateUploaderProps {
-  readonly discordSdk: DiscordSDK;
-}
-
-function TimerUploader({ discordSdk }: TimerStateUploaderProps) {
-  const [, setTimerState] = useState({
-    duration: 30 * 60000,
-    matchStartMillis: 0,
-    remainingMillis: 30 * 60000,
-    calledMillis: Infinity,
-    isRunning: false,
-  });
-  
-  function tick() {
-    const now = Date.now();
-    setTimerState(({duration, matchStartMillis, calledMillis, isRunning }) =>{
-      const newRemainingMillis = duration - now + matchStartMillis;
-
-      for (const { millis, text } of callTexts) {
-        if (millis < calledMillis && newRemainingMillis <= millis) {
-          say(text);
-          calledMillis = millis;
-          break;
-        }
-      }
-
-      if (newRemainingMillis < 0) {
-        return {
-          duration,
-          matchStartMillis,
-          remainingMillis: 0,
-          calledMillis,
-          isRunning: false,
-        };
-      }
-
-      if (isRunning) {
-        setTimeout(tick, 1000, Date.now());
-      }
-
-      const newTimerState = {
-        duration,
-        matchStartMillis,
-        remainingMillis: newRemainingMillis,
-        calledMillis,
-        isRunning,
-      };
-
-      fetch(`/.proxy/timerState/${discordSdk.instanceId}`, {
-        method: "PUT",
-        body: JSON.stringify(newTimerState),
-        headers: {
-          "Content-Type": "application/json",
-        },
-      });
-
-      return newTimerState;
-    });
-  }
-
-  function handleStart() {
-    const now = Date.now();
-
-    setTimerState(({duration, remainingMillis, calledMillis}) => ({
-      duration,
-      matchStartMillis: now,
-      remainingMillis,
-      calledMillis,
-      isRunning: true,
-    }));
-  
-    say(startingCallText);
-    setTimeout(tick, 1000, Date.now());
-  }
-
-  return (
-    <section>
-      <button onClick={handleStart}>スタート</button>
-    </section>
-  );
-}
-
 interface AppProps {
   discordSdk: DiscordSDK;
 }
 
 function App({discordSdk}: AppProps) {
-  const [editingTimerState, setEditingTimerState] = useState({
-    duration: 30 * 60000,
-    remainingMillis: 30 * 60000,
-    calledMillis: Infinity,
-    isRunning: false,
-    matchStart: 0,
-  });
-
-
+  console.log(discordSdk);
   const [timerState, setTimerState] = useState({
     duration: 30 * 60000,
     remainingMillis: 30 * 60000,
@@ -140,48 +42,66 @@ function App({discordSdk}: AppProps) {
     setTimeout(tick, 1000, Date.now());
   }
 
-  function tickUpdate(timestamp: number) {
-    const {duration, calledMillis, isRunning, matchStart} = timerState;
+  function tick(timestamp: number) {
+    setTimerState(({duration, calledMillis, isRunning, matchStart}) => {
+      const newRemainingMillis = duration - timestamp + matchStart;
 
-    const newRemainingMillis = duration - timestamp + matchStart;
-
-    for (const { millis, text } of callTexts) {
-      if (millis < calledMillis && newRemainingMillis <= millis) {
-        say(text);
-        calledMillis = millis;
-        break;
+      for (const { millis, text } of callTexts) {
+        if (millis < calledMillis && newRemainingMillis <= millis) {
+          say(text);
+          calledMillis = millis;
+          break;
+        }
       }
-    }
 
-    if (newRemainingMillis < 0) {
-      return {
+      if (newRemainingMillis < 0) {
+        return {
+          duration,
+          remainingMillis: 0,
+          calledMillis,
+          isRunning: false,
+          matchStart,
+        };
+      }
+
+      if (isRunning) {
+        setTimeout(tick, 1000, Date.now());
+      }
+
+      const nextPayload = {
         duration,
-        remainingMillis: 0,
+        remainingMillis: newRemainingMillis,
         calledMillis,
-        isRunning: false,
+        isRunning,
         matchStart,
       };
-    }
 
-    if (isRunning) {
-      setTimeout(tick, 1000, Date.now());
-    }
-
-    const nextPayload = {
-      duration,
-      remainingMillis: newRemainingMillis,
-      calledMillis,
-      isRunning,
-      matchStart,
-    };
-
-    fetch(`/.proxy/timerState/${discordSdk.instanceId}`, {
-      method: "PUT",
-      body: JSON.stringify(nextPayload),
-      headers: {
-        "Content-Type": "application/json",
-      },
+      return nextPayload;
     });
+  }
+
+  function handleStart() {
+    startTimer(Date.now());
+  }
+
+  function handlePlus() {
+    setTimerState((state) => {
+      return {
+        ...state,
+        duration: state.duration + 60000,
+        remainingMillis: state.remainingMillis + 60000,
+      }
+    })
+  }
+
+  function handleMinus() {
+    setTimerState((state) => {
+      return {
+        ...state,
+        duration: state.duration - 60000,
+        remainingMillis: state.remainingMillis + 60000,
+      }
+    })
   }
 
   return (
@@ -193,11 +113,14 @@ function App({discordSdk}: AppProps) {
       }}
       >
 
-      <TimerUploader />
+      <button onClick={handleStart}>スタート</button>
 
       <section>
         <h1>{Math.floor(timerState.remainingMillis / 60000).toString().padStart(2, '0')}:{Math.floor(timerState.remainingMillis % 60000 / 1000).toString().padStart(2, '0')}</h1>
       </section>
+
+      <button onClick={handleMinus}>-1分</button>
+      <button onClick={handlePlus}>+1分</button>
 
       <section>
         <h1>{discordSdk.instanceId}</h1>
