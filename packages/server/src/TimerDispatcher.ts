04770fa2d46ee6cd9b56d369a23eed541ff19904
Kaito Udagawa <umireon@gmail.com>
2025-04-19 06:37:33 +0900

    Fix

--- packages/server/src/TimerDispatcher.ts
+++ packages/server/src/TimerDispatcher.ts
@@ -1,31 +1,31 @@
-import { DurableObject } from "cloudflare:workers";
+// import { DurableObject } from "cloudflare:workers";
 
-export class TimerDispatcher extends DurableObject {
-  connections: Set<WebSocket> = new Set();
+// export class TimerDispatcher extends DurableObject {
+//   connections: Set<WebSocket> = new Set();
 
-  constructor(ctx: DurableObjectState, env: unknown) {
-    super(ctx, env);
-  }
+//   constructor(ctx: DurableObjectState, env: unknown) {
+//     super(ctx, env);
+//   }
 
-  async fetch(request: Request): Promise<Response> {
-    const { 0: client, 1: server } = new WebSocketPair();
-    console.error("client", client);
-    this.ctx.acceptWebSocket(client);
-    this.connections.add(client);
-    console.error("connections", this.connections.size);
-    return new Response(null, {
-      status: 101,
-      webSocket: client,
-    });
-  }
+//   async fetch(request: Request): Promise<Response> {
+//     const { 0: client, 1: server } = new WebSocketPair();
+//     console.error("client", client);
+//     this.ctx.acceptWebSocket(server);
+//     this.connections.add(client);
+//     console.error("connections", this.connections.size);
+//     return new Response(null, {
+//       status: 101,
+//       webSocket: client,
+//     });
+//   }
 
-  async dispatch(event: string): Promise<Response> {
-    console.error("Size", this.connections.size)
-    for (const connection of this.connections) {
-      connection.send(event);
-    }
-    return new Response(null, {
-      status: 200,
-    });
-  }
-}
+//   async dispatch(event: string): Promise<Response> {
+//     console.error("Size", this.connections.size)
+//     for (const connection of this.connections) {
+//       connection.send(event);
+//     }
+//     return new Response(null, {
+//       status: 200,
+//     });
+//   }
+// }
--- packages/server/src/index.ts
+++ packages/server/src/index.ts
@@ -1,10 +1,10 @@
 import { Hono } from "hono";
-import { TimerDispatcher } from "./TimerDispatcher.js";
+// import { TimerDispatcher } from "./TimerDispatcher.js";
 
 type Bindings = {
   CLIENT_ID: string;
   CLIENT_SECRET: string;
-  TIMER_DISPATCHER: DurableObjectNamespace<TimerDispatcher>;
+  // TIMER_DISPATCHER: DurableObjectNamespace<TimerDispatcher>;
 };
 
 const app = new Hono<{ Bindings: Bindings }>();
@@ -13,28 +13,28 @@ app.get("/", (c) => {
   return c.text("Hello Hono!");
 });
 
-app.get("/listen/:instanceId", async (c) => {
-  const upgradeHeader = c.req.header("Upgrade");
+// app.get("/listen/:instanceId", async (c) => {
+//   const upgradeHeader = c.req.header("Upgrade");
 
-  if (!upgradeHeader || upgradeHeader !== "websocket") {
-    return c.text("Expected websocket connection", 426);
-  }
+//   if (!upgradeHeader || upgradeHeader !== "websocket") {
+//     return c.text("Expected websocket connection", 426);
+//   }
 
-  const { instanceId } = c.req.param();
-  const timerDispatcherId = c.env.TIMER_DISPATCHER.idFromName(instanceId);
-  const timerDispatcher = c.env.TIMER_DISPATCHER.get(timerDispatcherId);
-  console.error("a", timerDispatcherId);
+//   const { instanceId } = c.req.param();
+//   const timerDispatcherId = c.env.TIMER_DISPATCHER.idFromName(instanceId);
+//   const timerDispatcher = c.env.TIMER_DISPATCHER.get(timerDispatcherId);
+//   console.error("a", timerDispatcherId);
 
-  return timerDispatcher.fetch(c.req.raw);
-});
+//   return timerDispatcher.fetch(c.req.raw);
+// });
 
-app.post("/dispatch/:instanceId", async (c) => {
-  const { instanceId } = c.req.param();
-  const timerDispatcherId = c.env.TIMER_DISPATCHER.idFromName(instanceId);
-  const timerDispatcher = c.env.TIMER_DISPATCHER.get(timerDispatcherId);
-  console.error("a", timerDispatcherId);
-  return timerDispatcher.dispatch(await c.req.text());
-});
+// app.post("/dispatch/:instanceId", async (c) => {
+//   const { instanceId } = c.req.param();
+//   const timerDispatcherId = c.env.TIMER_DISPATCHER.idFromName(instanceId);
+//   const timerDispatcher = c.env.TIMER_DISPATCHER.get(timerDispatcherId);
+//   console.error("a", timerDispatcherId);
+//   return timerDispatcher.dispatch(await c.req.text());
+// });
 
 export default app;
-export { TimerDispatcher };
+// export { TimerDispatcher };
