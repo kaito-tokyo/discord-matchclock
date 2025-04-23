b3fb0b54109842c4cd6723dd626fd8942943de5c
Kaito Udagawa <umireon@gmail.com>
2025-04-23 19:56:14 +0900

    Update index.ts

--- packages/server/src/index.ts
+++ packages/server/src/index.ts
@@ -8,7 +8,7 @@ import { EventRecorder } from "./EventRecorder.js";
 const app = new Hono<{ Bindings: Bindings }>();
 
 app.post("/", async (c) => {
-  const rawBody = await c.req.raw();
+  const rawBody = await c.req.arrayBuffer();
   const signature = c.req.header("X-Signature-Ed25519") ?? "";
   const timestamp = c.req.header("X-Signature-Timestamp") ?? "";
   const isValidRequest = verifyKey(rawBody, signature, timestamp, c.env.DISCORD_PUBLIC_KEY);
