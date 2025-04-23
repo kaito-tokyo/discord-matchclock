205833a0b6e66d1e019545a1019fd152ea960ec6
Kaito Udagawa <umireon@gmail.com>
2025-04-23 20:57:25 +0900

    Fix

--- packages/server/src/index.ts
+++ packages/server/src/index.ts
@@ -22,16 +22,13 @@ app.post("/", async (c) => {
   if (!signature) {
     return c.text("Missing signature!", 401);
   }
-  console.error("Signature", signature);
 
   const timestamp = c.req.header("X-Signature-Timestamp");
   if (!timestamp) {
     return c.text("Missing timestamp!", 401);
   }
-  console.error("Timestamp", timestamp);
 
   const rawBody = await c.req.arrayBuffer();
-  console.error("Body", await c.req.text());
 
   const isValidRequest = await verifyKey(rawBody, signature, timestamp, c.env.DISCORD_PUBLIC_KEY);
   if (!isValidRequest) {
@@ -41,37 +38,39 @@ app.post("/", async (c) => {
   const interaction = await c.req.json();
 
   if (interaction.type === InteractionType.PING) {
-    const { DISCORD_APPLICATION_ID, DISCORD_TOKEN } = c.env;
-    if (!DISCORD_APPLICATION_ID) {
-      throw new Error("DISCORD_APPLICATION_ID is not set");
-    } else if (!DISCORD_TOKEN) {
-      throw new Error("DISCORD_TOKEN is not set");
-    }
-
-    const response = await fetch(
-      `https://discord.com/api/v10/applications/${DISCORD_APPLICATION_ID}/commands`,
-      {
-        headers: {
-          "Content-Type": "application/json",
-          Authorization: `Bot ${DISCORD_TOKEN}`,
-        },
-        method: "PUT",
-        body: JSON.stringify([MATCHCLOCK_COMMAND]),
-      },
-    );
-
-    if (response.ok) {
-      console.log("Successfully registered global command");
-    } else {
-      console.error("Failed to register global command", response);
-      throw new Error("Failed to register global command");
-    }
-
     return c.json({ type: InteractionResponseType.PONG });
   } else {
     return c.text("Unknown interaction type!", 400);
   }
 });
+
+app.post("/register", async (c) => {
+  const { DISCORD_APPLICATION_ID, DISCORD_TOKEN } = c.env;
+  if (!DISCORD_APPLICATION_ID) {
+    throw new Error("DISCORD_APPLICATION_ID is not set");
+  } else if (!DISCORD_TOKEN) {
+    throw new Error("DISCORD_TOKEN is not set");
+  }
+
+  const response = await fetch(
+    `https://discord.com/api/v10/applications/${DISCORD_APPLICATION_ID}/commands`,
+    {
+      headers: {
+        "Content-Type": "application/json",
+        Authorization: `Bot ${DISCORD_TOKEN}`,
+      },
+      method: "PUT",
+      body: JSON.stringify([MATCHCLOCK_COMMAND]),
+    },
+  );
+
+  if (response.ok) {
+    console.log("Successfully registered global command");
+  } else {
+    console.error("Failed to register global command", await response.text());
+    throw new Error("Failed to register global command");
+  }
+})
 
 app.post("/timerEvents/:instanceId", async (c) => {
   const { instanceId } = c.req.param();
