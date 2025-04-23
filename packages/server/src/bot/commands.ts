8321d9b5f09dfd9d2974d6f602c6eb6f2d89a0e8
Kaito Udagawa <umireon@gmail.com>
2025-04-23 21:29:11 +0900

    Fix

--- packages/server/src/bot/commands.ts
+++ packages/server/src/bot/commands.ts
@@ -1,4 +1,9 @@
+export const LAUNCH_COMMAND = {
+  name: "launch",
+  description: "Launch an activity",
+};
+
 export const MATCHCLOCK_COMMAND = {
   name: "matchclock",
-  description: "Configure Matchclock.",
+  description: "Configure Matchclock",
 };
